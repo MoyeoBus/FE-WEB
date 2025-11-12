f9cbfc884559e62fe58111ff7ba526c63225c828
UHyeonj <dngusdlqwkd@gmail.com>
2025-11-12 15:32:53 +0900

    feat:  운수사 메인 차트 구현

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,6 +6,9 @@ import PassengerStatus from '../pages/PassengerStatus';
 import RouteStatus from '../pages/RouteStatus';
 import Home from '../pages/Home';
 
+//operator pages
+import DataAnalysis from '../pages/DataAnalysis';
+
 const routes = createBrowserRouter([
   {
     path: '/',
@@ -30,6 +33,24 @@ const routes = createBrowserRouter([
       {
         path: 'routeStatus',
         element: <RouteStatus />,
+      },
+    ],
+  },
+  {
+    path: 'operator',
+    element: (
+      <MainLayout>
+        <Outlet />
+      </MainLayout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <DataAnalysis />,
+      },
+      {
+        path: 'dataAnalysis',
+        element: <DataAnalysis />,
       },
     ],
   },
